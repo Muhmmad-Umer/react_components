5399c473af8467002b8ae5ac75f9a344cf7bd9ce
Muhammad Umer <umertariq7a@gmail.com>
2024-03-27 20:51:20 +0500

    Added Accordion Page

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,28 +1,3 @@
-import Accordion from "./components/Accordion";
-
-function App() {
-  const items = [
-    {
-      id: 1,
-      label: "Can I use react on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. ",
-    },
-    {
-      id: 2,
-      label: "Can I use react on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. ",
-    },
-    {
-      id: 3,
-      label: "Can I use react on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. ",
-    },
-  ];
-
-  return <Accordion items={items} />;
-}
+function App() {}
 
 export default App;
