5e53ba851c48d5063fadcda5ad599f8f8af8571b
Muhammad Umer <umertariq7a@gmail.com>
2024-03-27 17:55:26 +0500

    Created Button and Accordion

---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,44 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button success className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Hi
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Buy now
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoCloudDownload />
+          Bruasdasdav
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          Yeeee
+        </Button>
+      </div>
+      <div>
+        <Button danger>Kekw</Button>
+      </div>
+      <div>
+        <Button>GG</Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
